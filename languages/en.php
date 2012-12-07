48ec41d9cf6520156f64f5a2a0ceabf5a15aad81
Diego Andrés Ramírez Aragón <lowfill@gmail.com>
2012-12-07 13:22:12 +0100

    Added support for themes and language

--- languages/en.php
+++ languages/en.php
@@ -15,6 +15,18 @@ $english = array(
 	    You can get a different challenge by clicking the refresh button indise the image',
     'recaptcha:signup' => 'Please sign up for recaptcha to get you public and private keys at %s',
     'recaptcha:verified' => 'Verified',
+	'recaptcha:theme'=>'Select recaptcha theme',	
+
+	'recaptcha:captchafail' => "The reCAPTCHA wasn't entered correctly. Go back and try it again.",
+	'recaptcha:instructions_visual' => "Type the two words:",
+	'recaptcha:instructions_audio' => "Type what you hear:",
+	'recaptcha:play_again' => "Play sound again",
+	'recaptcha:cant_hear_this' => "Download sound as MP3",
+	'recaptcha:visual_challenge' => "Get a visual challenge",
+	'recaptcha:audio_challenge' => "Get an audio challenge",
+	'recaptcha:refresh_btn' => "Get a new challenge",
+	'recaptcha:help_btn' => "Help",
+	'recaptcha:incorrect_try_again' => "Incorrect. Try again.",		
 );
 
 add_translation("en", $english);
--- views/default/input/captcha.php
+++ views/default/input/captcha.php
@@ -7,9 +7,6 @@
  *
  */
 
-//add the recaptcha only if it has been enabled in the plugin setting
-$plugin_entity = elgg_get_plugin_from_id('recaptcha');
-$plugin_settings = $plugin_entity->getAllSettings();
 
 if(array_key_exists('recaptcha_verified', $_SESSION) && $_SESSION['recaptcha_verified'] == 1) {
 
@@ -17,7 +14,7 @@ if(array_key_exists('recaptcha_verified', $_SESSION) && $_SESSION['recaptcha_ver
    $output = "<label>".elgg_echo('recaptcha:label:human_verification')."</label><b>".elgg_echo('recaptcha:verified')."</b><br><br>";
 }
 else {
-    if($plugin_settings['require_recaptcha'] == 'on') {
+    if(elgg_get_plugin_setting('require_recaptcha','recaptcha') == 'on'){
 
         // include the recaptcha lib
         require_once(elgg_get_plugins_path() . 'recaptcha/lib/recaptchalib.php');
@@ -26,12 +23,52 @@ else {
         $use_ssl = false;
         if( isset($_SERVER['HTTPS']) && $_SERVER["HTTPS"] == "on" ) $use_ssl = true; 
                
-        $publickey = $plugin_settings['recaptcha_public_key'];
+        $publickey = elgg_get_plugin_setting('recaptcha_public_key','recaptcha');
         $output = "<label>".elgg_echo('recaptcha:label:human_verification')."</label><br>";
+
+        $lang = elgg_trigger_plugin_hook('lang', 'captcha');
+        if(empty($lang)){
+        	$lang = 'en';
+        }
+        
+        //TODO Add this to plugin settings
+        $theme = elgg_get_plugin_setting('recaptcha_theme','recaptcha');
+        if(empty($theme)){
+        	$theme = 'clean';
+        }
+        
+        if(!empty($vars['theme'])){
+        	$theme = 'custom';
+        	$custom_theme_widget = $vars['theme']."_recaptcha_theme";
+        	$output .= elgg_view('input/captcha_'.$vars['theme'].'_theme');
+        }
+        
+        $output .= "<script type='text/javascript'>
+                var RecaptchaOptions = {
+                        custom_translations : {
+                                        instructions_visual : '" . elgg_echo('recaptcha:instructions_visual') . "',
+                                        instructions_audio : '" . elgg_echo('recaptcha:instructions_audio') . "',
+                                        play_again : '" . elgg_echo('recaptcha:play_again') . "',
+                                        cant_hear_this : '" . elgg_echo('recaptcha:cant_here_this') . "',
+                                        visual_challenge : '" . elgg_echo('recaptcha:visual_challenge') . "',
+                                        audio_challenge : '" . elgg_echo('recaptcha:audio_challenge') . "',
+                                        refresh_btn : '" . elgg_echo('recaptcha:refresh_btn') . "',
+                                        help_btn : '" . elgg_echo('recaptcha:help_btn') . "',
+                                        incorrect_try_again : '" . elgg_echo('recaptcha:incorrect_try_again') . "'
+                                        },
+                        lang : '" . $lang . "', // Unavailable while writing this code (just for audio challenge)
+                        theme : '" . $theme . "',
+                        custom_theme_widget: '{$custom_theme_widget}'
+                    };
+                </script>
+        ";
+        
         $output .= recaptcha_get_html($publickey, null, $use_ssl);
         $output .= '<br>';
     }
-    else $output = '';
+    else {
+    	$output = '';
+    }
 }
 
 echo $output;
